d4b26ef0781e7ec43fbdc3b438d57eb9f38a5b43
TheoKaprel <theo.kaprelian@creatis.insa-lyon.fr>
2025-04-22 11:48:20 +0200

    only autograd recons and likeimg instd of unused attmap

--- rtk_mlem_autograd.py
+++ rtk_mlem_autograd.py
@@ -6,87 +6,13 @@ import itk
 import numpy as np
 import torch
 
-class SPECT_system_rtk:
-    def __init__(self, projections_fn, attmap_fn, geom_fn):
-        self.Dimension = 3
-        self.pixelType = itk.F
-        self.imageType = itk.Image[self.pixelType, self.Dimension]
-
-        self.projection_fn = projections_fn
-        self.attmap_fn = attmap_fn
-
-        self.init_images()
-
-        xmlReader = rtk.ThreeDCircularProjectionGeometryXMLFileReader.New()
-        xmlReader.SetFilename(geom_fn)
-        xmlReader.GenerateOutputInformation()
-        self.geometry = xmlReader.GetOutputObject()
-        self.joseph_forward_projector = rtk.JosephForwardProjectionImageFilter[self.imageType,self.imageType].New()
-        self.joseph_forward_projector.SetGeometry(self.geometry)
-
-        self.joseph_back_projector = rtk.JosephBackProjectionImageFilter[self.imageType,self.imageType].New()
-        self.joseph_back_projector.SetGeometry(self.geometry)
-
-    def init_images(self):
-        self.projection_itkimg = itk.imread(self.projection_fn,self.pixelType)
-        self.attmap_itkimg = itk.imread(self.attmap_fn,self.pixelType)
-
-        self.projection_array = itk.array_from_image(self.projection_itkimg)
-        self.attmap_array = itk.array_from_image(self.attmap_itkimg)
-
-    def forward(self, image_array):
-        image_itkimg = itk.image_from_array(image_array)
-        image_itkimg.CopyInformation(self.attmap_itkimg)
-
-        self.joseph_forward_projector.SetInput(1, image_itkimg)
-        zero_proj_array = np.zeros_like(self.projection_array)
-        zero_proj_itkimg = itk.image_from_array(zero_proj_array)
-        zero_proj_itkimg.CopyInformation(self.projection_itkimg)
-        self.joseph_forward_projector.SetInput(0, zero_proj_itkimg)
-        self.joseph_forward_projector.Update()
-        forward_projected_itkimg = self.joseph_forward_projector.GetOutput()
-        return itk.array_from_image(forward_projected_itkimg)
-
-    def backward(self, projections_array):
-        projections_itkimg = itk.image_from_array(projections_array)
-        projections_itkimg.CopyInformation(self.projection_itkimg)
-        zero_img_array = np.zeros_like(self.attmap_array)
-        zero_img_itkimg = itk.image_from_array(zero_img_array)
-        zero_img_itkimg.CopyInformation(self.attmap_itkimg)
-        self.joseph_back_projector.SetInput(0, zero_img_itkimg)
-        self.joseph_back_projector.SetInput(1, projections_itkimg)
-        self.joseph_back_projector.Update()
-        image_itkimg = self.joseph_back_projector.GetOutput()
-        return itk.array_from_image(image_itkimg)
-
-    def init_mlem(self):
-        ones_projection_array = np.ones_like(self.projection_itkimg)
-        ones_projection_itkimg = itk.image_from_array(ones_projection_array)
-        ones_projection_itkimg.CopyInformation(self.projection_itkimg)
-
-        zero_img_array = np.zeros_like(self.attmap_array)
-        zero_img_itkimg = itk.image_from_array(zero_img_array)
-        zero_img_itkimg.CopyInformation(self.attmap_itkimg)
-
-        self.joseph_back_projector.SetInput(0, zero_img_itkimg)
-        self.joseph_back_projector.SetInput(1, ones_projection_itkimg)
-        self.joseph_back_projector.Update()
-        bp_ones_itkimg = self.joseph_back_projector.GetOutput()
-        self.bp_ones_array = itk.array_from_image(bp_ones_itkimg)
-        self.bp_ones_array[self.bp_ones_array==0] = 1
-
-    def mlem_iter(self, image_array):
-        fp_image_array = self.forward(image_array)
-        fp_image_array[fp_image_array==0] = 1
-        return image_array  * self.backward(self.projection_array/fp_image_array) / self.bp_ones_array
-
 class forwardprojection(torch.autograd.Function):
     @staticmethod
     def forward(image_tensor, spect_model):
         device = image_tensor.device
         image_array = image_tensor.cpu().numpy()
         image_itkimg = itk.image_from_array(image_array)
-        image_itkimg.CopyInformation(spect_model.attmap_itkimg)
+        image_itkimg.CopyInformation(spect_model.like_itkimg)
 
         spect_model.joseph_forward_projector.SetInput(1, image_itkimg)
         zero_proj_array = np.zeros_like(spect_model.projection_array) #
@@ -110,9 +36,9 @@ class forwardprojection(torch.autograd.Function):
         projections_array = projections_tensor.cpu().numpy()
         projections_itkimg = itk.image_from_array(projections_array)
         projections_itkimg.CopyInformation(spect_model.projection_itkimg)
-        zero_img_array = np.zeros_like(spect_model.attmap_array) #
+        zero_img_array = np.zeros_like(spect_model.like_array) #
         zero_img_itkimg = itk.image_from_array(zero_img_array) #
-        zero_img_itkimg.CopyInformation(spect_model.attmap_itkimg) #
+        zero_img_itkimg.CopyInformation(spect_model.like_itkimg) #
         spect_model.joseph_back_projector.SetInput(0, zero_img_itkimg) #
         spect_model.joseph_back_projector.SetInput(1, projections_itkimg)
         spect_model.joseph_back_projector.Update()
@@ -123,7 +49,7 @@ class forwardprojection(torch.autograd.Function):
 
 
 class SPECT_system_torch(torch.nn.Module):
-    def __init__(self, projections_fn, attmap_fn, geom_fn):
+    def __init__(self, projections_fn, like_fn, geom_fn):
         super().__init__()
 
         self.Dimension = 3
@@ -131,7 +57,7 @@ class SPECT_system_torch(torch.nn.Module):
         self.imageType = itk.Image[self.pixelType, self.Dimension]
 
         self.projection_fn = projections_fn
-        self.attmap_fn = attmap_fn
+        self.like_fn = like_fn
 
         self.init_images()
 
@@ -147,23 +73,23 @@ class SPECT_system_torch(torch.nn.Module):
 
     def init_images(self):
         self.projection_itkimg = itk.imread(self.projection_fn,self.pixelType)
-        self.attmap_itkimg = itk.imread(self.attmap_fn,self.pixelType)
+        self.like_itkimg = itk.imread(self.like_fn,self.pixelType)
 
         self.projection_array = itk.array_from_image(self.projection_itkimg)
-        self.attmap_array = itk.array_from_image(self.attmap_itkimg)
+        self.like_array = itk.array_from_image(self.like_itkimg)
 
 
 
 def main():
     print(args)
 
-    spect = SPECT_system_torch(projections_fn=args.projections,attmap_fn=args.attmap,geom_fn=args.geom)
+    spect = SPECT_system_torch(projections_fn=args.projections,like_fn=args.like,geom_fn=args.geom)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     print(f"Device : {device}")
 
-    image_k_array = np.ones_like(spect.attmap_itkimg)
+    image_k_array = np.ones_like(spect.like_itkimg)
     image_k_tensor = torch.tensor(image_k_array, device=device)
     image_k_tensor.requires_grad_(True)
 
@@ -185,14 +111,14 @@ def main():
 
         image_k_array = image_k_tensor.detach().cpu().numpy()
         image_k_itkimg = itk.image_from_array(image_k_array)
-        image_k_itkimg.CopyInformation(spect.attmap_itkimg)
+        image_k_itkimg.CopyInformation(spect.like_itkimg)
         itk.imwrite(image_k_itkimg, "test_autograd/rec_torch_{}.mha".format(iteration))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--projections")
-    parser.add_argument("--attmap")
+    parser.add_argument("--likeimg")
     parser.add_argument("--geom")
     parser.add_argument("--output")
     parser.add_argument("--niter", type = int, default=10)
